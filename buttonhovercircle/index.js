4ab39dd13b43a546b15f4c292f1c0c535db0c248
Valentin Weiß <valentin.weiss@hahn-softwareentwicklung.de>
2021-01-06 00:55:10 +0100

    added Hex

--- buttonhovercircle/index.js
+++ buttonhovercircle/index.js
@@ -1,25 +1,28 @@
 window.onload = function () {
   let buttons = document.getElementsByClassName("btn");
   console.log(buttons);
+  let num = 1;
   for (const btn of buttons) {
     btn.addEventListener("mouseenter", (e) => {
       var rect = e.target.getBoundingClientRect();
       var x = e.clientX - rect.left;
       var y = e.clientY - rect.top;
       var size = Math.max(rect.width, rect.height) * 2.3;
-      e.target.childNodes[0].style.left = x + "px";
-      e.target.childNodes[0].style.top = y + "px";
-      e.target.childNodes[0].style.width = size + "px";
-      e.target.childNodes[0].style.height = size + "px";
+      console.log(e.target);
+      console.log(e.target.childNodes);
+      e.target.childNodes[num].style.left = x + "px";
+      e.target.childNodes[num].style.top = y + "px";
+      e.target.childNodes[num].style.width = size + "px";
+      e.target.childNodes[num].style.height = size + "px";
     });
     btn.addEventListener("mouseleave", (e) => {
       var rect = e.target.getBoundingClientRect();
       var x = e.clientX - rect.left;
       var y = e.clientY - rect.top;
-      e.target.childNodes[0].style.left = x + "px";
-      e.target.childNodes[0].style.top = y + "px";
-      e.target.childNodes[0].style.width = "0px";
-      e.target.childNodes[0].style.height = "0px";
+      e.target.childNodes[num].style.left = x + "px";
+      e.target.childNodes[num].style.top = y + "px";
+      e.target.childNodes[num].style.width = "0px";
+      e.target.childNodes[num].style.height = "0px";
     });
   }
 };
