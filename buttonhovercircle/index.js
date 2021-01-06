7e90866766daab2dda1532b6fd5d442870836d43
Valentin Weiß <valentin.weiss@hahn-softwareentwicklung.de>
2021-01-06 01:35:09 +0100

    added hexagon back input field

--- buttonhovercircle/index.js
+++ buttonhovercircle/index.js
@@ -8,8 +8,6 @@ window.onload = function () {
       var x = e.clientX - rect.left;
       var y = e.clientY - rect.top;
       var size = Math.max(rect.width, rect.height) * 2.3;
-      console.log(e.target);
-      console.log(e.target.childNodes);
       e.target.childNodes[num].style.left = x + "px";
       e.target.childNodes[num].style.top = y + "px";
       e.target.childNodes[num].style.width = size + "px";
@@ -23,6 +21,38 @@ window.onload = function () {
       e.target.childNodes[num].style.top = y + "px";
       e.target.childNodes[num].style.width = "0px";
       e.target.childNodes[num].style.height = "0px";
+    });
+  }
+
+  let inputs = document.getElementsByClassName("inputstyleinput");
+  console.log(inputs);
+  for (const btn of inputs) {
+    let curX = null;
+    let curY = null;
+    btn.addEventListener("mousemove", (e) => {
+      curX = e.clientX;
+      curY = e.clientY;
+    });
+    btn.addEventListener("focusin", (e) => {
+      var rect = e.target.getBoundingClientRect();
+      var x = curX - rect.left;
+      var y = curY - rect.top;
+      var size = Math.max(rect.width, rect.height) * 2.3;
+      let svg = e.target.parentNode.childNodes[1];
+      svg.style.left = x + "px";
+      svg.style.top = y + "px";
+      svg.style.width = size + "px";
+      svg.style.height = size + "px";
+    });
+    btn.addEventListener("focusout", (e) => {
+      var rect = e.target.getBoundingClientRect();
+      var x = curX - rect.left;
+      var y = curY - rect.top;
+      let svg = e.target.parentNode.childNodes[1];
+      svg.style.left = x + "px";
+      svg.style.top = y + "px";
+      svg.style.width = "0px";
+      svg.style.height = "0px";
     });
   }
 };
